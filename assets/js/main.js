efcd6467f81591dd4cbb3a00e8d1ec23acbdef7a
Naziur-Rahman <naziurr85@gmail.com>
2024-06-28 22:50:32 +0600

    added main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+// Js change options are bellow:-
+
+// selection
+let home = document.querySelectorAll("#home");
+
+// css change
+home.style.color = "red";
+// html change
+
+// adding listeners
